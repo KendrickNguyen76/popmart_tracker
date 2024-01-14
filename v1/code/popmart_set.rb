cf76a98db76c469962eb22d08ce24cb7baa222d9
KendrickNguyen76 <kendricknguyen76@gmail.com>
2024-01-14 13:07:30 -0800

    Added popmart_set.rb and its test file

---
+++ v1/code/popmart_set.rb
@@ -0,0 +1,4 @@
+# popmart_set.rb
+
+# This file contains the PopmartSet class. It is used to represent a singular popmart
+# set, and will be utilized in many different parts of the program overall.
---
+++ v1/tests/popmart_set_tests.rb
@@ -0,0 +1,5 @@
+# popmart_set_tests.rb
+
+# This file contains all of the tests for the PopmartSet class, which is located
+# in the popmart_set.rb file. This will be used to test the functionality of the class
+# and ensure that everything is working properly.
