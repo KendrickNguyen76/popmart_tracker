143f5d09dd9c5900e280bb5fefdade03cbb292f2
KendrickNguyen76 <kendricknguyen76@gmail.com>
2024-01-14 13:56:57 -0800

    Wrote PopMartSet constructor w/ tests

--- v1/code/popmart_set.rb
+++ v1/code/popmart_set.rb
@@ -1,4 +1,26 @@
 # popmart_set.rb
 
-# This file contains the PopmartSet class. It is used to represent a singular popmart
+# This file contains the PopMartSet class. It is used to represent a singular popmart
 # set, and will be utilized in many different parts of the program overall.
+
+class PopMartSet
+    # PopMartSet is a class that represents a singular Pop Mart set of blindboxes
+
+    # It has three instance variables:
+
+    # @brand - the brand/character of the set (ex. DIMOO, SkullPanda, etc.)
+    # @series - the series of the set (ex. Vacation, Valentines Day, etc.)
+    # @price - the price of the set in total
+    # @figures - an array of all the figures in the Popmart set
+    attr_accessor :price
+    attr_reader :brand, :series_name, :figures
+
+
+    # The constructor for a PopMartSet object
+    def initialize(brand, series_name, price = 0.0)
+        @brand = brand.upcase
+        @series_name = series_name.upcase
+        @price = price
+    end
+
+end
--- v1/tests/popmart_set_tests.rb
+++ v1/tests/popmart_set_tests.rb
@@ -1,5 +1,23 @@
 # popmart_set_tests.rb
 
-# This file contains all of the tests for the PopmartSet class, which is located
+# This file contains all of the tests for the PopMartSet class, which is located
 # in the popmart_set.rb file. This will be used to test the functionality of the class
 # and ensure that everything is working properly.
+
+# Require Statements
+require_relative "../code/popmart_set.rb"
+require "test/unit"
+
+class TestPopMartSet < Test::Unit::TestCase
+    # TestPopMartSet contains the test cases for the PopMartSet class
+
+
+    # Tests the PopMartSet constructor without giving it a specific price
+    def test_constructor_without_price
+        test_set = PopMartSet.new("TestBrand", "TestSeries")
+
+        assert_equal(test_set.brand, "TESTBRAND")
+        assert_equal(test_set.series_name, "TESTSERIES")
+    end
+
+end
