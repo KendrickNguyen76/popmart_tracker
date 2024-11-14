89f0ea53df7b11385a265aeb2086aac858b267ec
Kendrick Linux <kendricknguyen76@gmail.com>
2024-11-13 22:31:29 -0800

    Removed file_path from PopTrackLogic

--- v1/tests/popmart_tracker_logic_tests.rb
+++ v1/tests/popmart_tracker_logic_tests.rb
@@ -15,7 +15,7 @@ class TestPopTrackLogic < Test::Unit::TestCase
 
     # Tests that is_valid_command() returns true or false correctly depending on its input
     def test_is_valid_command_returns_correct_value
-        test_tracker = PopTrackLogic.new("Test/Path")
+        test_tracker = PopTrackLogic.new
 
         assert_true(test_tracker.is_valid_command?("add set"));
         assert_true(test_tracker.is_valid_command?("ADD SET"));
@@ -24,7 +24,7 @@ class TestPopTrackLogic < Test::Unit::TestCase
 
     # Tests that add_set() allows you to add a set to PopTrackLogic
     def test_can_add_pop_mart_set_objects_to_class
-        test_tracker = PopTrackLogic.new("Test/Path")
+        test_tracker = PopTrackLogic.new
         
         assert_equal(test_tracker.sets.size, 0)
         
@@ -35,4 +35,19 @@ class TestPopTrackLogic < Test::Unit::TestCase
         assert_equal(test_tracker.sets["BRAND_SERIES NAME"].series_name, "Series Name")
         assert_equal(test_tracker.sets["BRAND_SERIES NAME"].price, 0.0)
     end
+	
+	# Tests that add_to_specific_set() allows you to add a figure to
+	# a specific set
+	def test_can_add_pop_mart_figure_to_specific_set
+		test_tracker = PopTrackLogic.new
+		test_set = PopMartSet.new("Brand", "Series Name")
+		test_figure = PopMartFigure.new("name", 1/2, true)
+
+		test_tracker.add_set(test_set)
+		test_tracker.add_to_specific_set("BRAND_SERIES NAME", test_figure)
+		find_result = test_tracker.sets["BRAND_SERIES NAME"].find_figure(test_figure.name)
+		
+		assert_equal(find_result.name, test_figure.name)
+	end
 end
+
