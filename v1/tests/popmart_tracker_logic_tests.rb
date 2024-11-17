13c0265651943bd092df23c9b48c2f021e4f3746
Kendrick Linux <kendricknguyen76@gmail.com>
2024-11-17 01:35:06 -0800

    Added setup method to popmart_tracker tests to refactor

--- v1/tests/popmart_tracker_logic_tests.rb
+++ v1/tests/popmart_tracker_logic_tests.rb
@@ -11,41 +11,41 @@ require "test/unit"
 
 class TestPopTrackLogic < Test::Unit::TestCase
     # TestPopTrackLogic contains the test cases for the PopTrackLogic class
-
+	
+	
+	# Create a PopTrackLogic object called test_tracker before each test.
+	def setup
+		@test_tracker = PopTrackLogic.new
+	end
 
     # Tests that is_valid_command() returns true or false correctly depending on its input
     def test_is_valid_command_returns_correct_value
-        test_tracker = PopTrackLogic.new
-
-        assert_true(test_tracker.is_valid_command?("add set"));
-        assert_true(test_tracker.is_valid_command?("ADD SET"));
-        assert_false(test_tracker.is_valid_command?("invalid"));
+        assert_true(@test_tracker.is_valid_command?("add set"));
+        assert_true(@test_tracker.is_valid_command?("ADD SET"));
+        assert_false(@test_tracker.is_valid_command?("invalid"));
     end
 
     # Tests that add_set() allows you to add a set to PopTrackLogic
     def test_can_add_pop_mart_set_objects_to_class
-        test_tracker = PopTrackLogic.new
+        assert_equal(@test_tracker.sets.size, 0)
         
-        assert_equal(test_tracker.sets.size, 0)
-        
-        test_tracker.add_set(PopMartSet.new("Brand", "Series Name"))
+        @test_tracker.add_set(PopMartSet.new("Brand", "Series Name"))
 
-        assert_equal(test_tracker.sets.size, 1)
-        assert_equal(test_tracker.sets["BRAND_SERIES NAME"].brand, "Brand")
-        assert_equal(test_tracker.sets["BRAND_SERIES NAME"].series_name, "Series Name")
-        assert_equal(test_tracker.sets["BRAND_SERIES NAME"].price, 0.0)
+        assert_equal(@test_tracker.sets.size, 1)
+        assert_equal(@test_tracker.sets["BRAND_SERIES NAME"].brand, "Brand")
+        assert_equal(@test_tracker.sets["BRAND_SERIES NAME"].series_name, "Series Name")
+        assert_equal(@test_tracker.sets["BRAND_SERIES NAME"].price, 0.0)
     end
 	
 	# Tests that add_to_specific_set() allows you to add a figure to
 	# a specific set
 	def test_can_add_pop_mart_figure_to_specific_set
-		test_tracker = PopTrackLogic.new
 		test_set = PopMartSet.new("Brand", "Series Name")
 		test_figure = PopMartFigure.new("name", 1/2, true)
 
-		test_tracker.add_set(test_set)
-		test_tracker.add_to_specific_set("BRAND_SERIES NAME", test_figure)
-		find_result = test_tracker.sets["BRAND_SERIES NAME"].find_figure(test_figure.name)
+		@test_tracker.add_set(test_set)
+		@test_tracker.add_to_specific_set("BRAND_SERIES NAME", test_figure)
+		find_result = @test_tracker.sets["BRAND_SERIES NAME"].find_figure(test_figure.name)
 		
 		assert_equal(find_result.name, test_figure.name)
 	end
