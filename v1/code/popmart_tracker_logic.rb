19dc3ee462b78237f9e3f2fd98b2375f43efd1f9
Kendrick Linux <kendricknguyen76@gmail.com>
2024-11-17 02:18:13 -0800

    Began working on modify_set_name()

--- v1/code/popmart_tracker_logic.rb
+++ v1/code/popmart_tracker_logic.rb
@@ -57,6 +57,12 @@ class PopTrackLogic
 			raise ArgumentError.new("Set with name #{series_name} and brand #{brand_name} does not exist")
 		end
 	end
+	
+	# Needs a PopMartSet object and a string representing the new name. 
+	# Allows them to change the name of the given set.
+	def modify_set_name(set, new_name)
+		# Nothing yet!
+	end
 end
 
 # Notes:
