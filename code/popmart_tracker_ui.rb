9a3e5efaf066434e5587d5ce8d024e724caedfcc
Kendrick Linux <kendricknguyen76@gmail.com>
2024-11-24 16:13:12 -0800

    Began adding more functionality to UI, not tested yet

--- code/popmart_tracker_ui.rb
+++ code/popmart_tracker_ui.rb
@@ -18,11 +18,23 @@ class PopTrackUI
 	# Constructor for a PopTrackUI object
 	def initialize
 		@tracker = PopTrackLogic.new
+		@running = true
 		print_start_up()
+		run_tracker()
 	end
-
+	
+	# Prints the start up message for a popmart tracker program
 	def print_start_up
 		puts "Welcome to the Popmart Tracker!"
 		puts "Please type in \"HELP\" if you need assistance"
+	end
+	
+	# Runs the popmart tracker. Gets user inputs and carries out
+	# specific commands based on the input
+	def run_tracker
+		while @running
+			input = gets.chomp
+			puts input
+		end
 	end
 end
