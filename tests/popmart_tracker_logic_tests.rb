5b69506186c84c8ddbbc5e7eb29a9d5ae131de91
Kendrick Linux <kendricknguyen76@gmail.com>
2024-12-23 01:20:04 -0800

    Fixed spacing issues in tracker logic tests

--- tests/popmart_tracker_logic_tests.rb
+++ tests/popmart_tracker_logic_tests.rb
@@ -13,18 +13,18 @@ class TestPopTrackLogic < Test::Unit::TestCase
     # TestPopTrackLogic contains the test cases for the PopTrackLogic class
 	
 	
-	# Create a PopTrackLogic object called test_tracker before each test.
-	def setup
-		@test_tracker = PopTrackLogic.new
-		@test_set = PopMartSet.new("Brand", "Series Name")
-	end
-	
-	# Tests that generate_dict_key returns the correct result
-	def test_generate_dict_key_creates_key_in_correct_format
-		test_key = @test_tracker.generate_dict_key(@test_set.brand, @test_set.series_name)
+    # Create a PopTrackLogic object called test_tracker before each test.
+    def setup
+        @test_tracker = PopTrackLogic.new
+        @test_set = PopMartSet.new("Brand", "Series Name")
+    end
 
-		assert_equal(test_key, "BRAND_SERIES NAME")
-	end
+    # Tests that generate_dict_key returns the correct result
+    def test_generate_dict_key_creates_key_in_correct_format
+        test_key = @test_tracker.generate_dict_key(@test_set.brand, @test_set.series_name)
+
+        assert_equal(test_key, "BRAND_SERIES NAME")
+    end
 
     # Tests that add_set() allows you to add a set to PopTrackLogic
     def test_can_add_pop_mart_set_objects_to_class
@@ -38,32 +38,34 @@ class TestPopTrackLogic < Test::Unit::TestCase
         assert_equal(@test_tracker.sets["BRAND_SERIES NAME"].price, 0.0)
     end
 	
-	# Tests that add_to_specific_set() allows you to add a figure to
-	# a specific set
-	def test_can_add_pop_mart_figure_to_specific_set
-		test_figure = PopMartFigure.new("name", 1/2, true)
+    # Tests that add_to_specific_set() allows you to add a figure to
+    # a specific set
+    def test_can_add_pop_mart_figure_to_specific_set
+        test_figure = PopMartFigure.new("name", 1/2, true)
 
-		@test_tracker.add_set(@test_set)
-		@test_tracker.add_to_specific_set("BRAND_SERIES NAME", test_figure)
-		find_result = @test_tracker.sets["BRAND_SERIES NAME"].find_figure(test_figure.name)
-		
-		assert_equal(find_result.name, test_figure.name)
-	end
+        @test_tracker.add_set(@test_set)
+        @test_tracker.add_to_specific_set("BRAND_SERIES NAME", test_figure)
+        find_result = @test_tracker.sets["BRAND_SERIES NAME"].find_figure(test_figure.name)
 
-	# Tests that get_set() correctly returns correct set
-	def test_get_set_returns_set_when_found
-		@test_tracker.add_set(@test_set)
+        assert_equal(find_result.name, test_figure.name)
+    end
 
-		get_result = @test_tracker.get_set("brand", "series name")
+    # Tests that get_set() correctly returns correct set
+    def test_get_set_returns_set_when_found
+        @test_tracker.add_set(@test_set)
 
-		assert_equal(get_result.brand, @test_set.brand)
-		assert_equal(get_result.series_name, @test_set.series_name)
-	end
+        get_result = @test_tracker.get_set("brand", "series name")
 
-	# Tests that get_set() throws an ArgumentError when given
-	# a test that doesn't exist in PopTrackLogic
-	def test_get_set_throws_argument_error_when_set_not_found
-		assert_raise_message("Set with name Exist and brand Doesnt does not exist") {@test_tracker.get_set("Doesnt", "Exist")}
-	end
+        assert_equal(get_result.brand, @test_set.brand)
+        assert_equal(get_result.series_name, @test_set.series_name)
+    end
+
+    # Tests that get_set() throws an ArgumentError when given
+    # a test that doesn't exist in PopTrackLogic
+    def test_get_set_throws_argument_error_when_set_not_found
+        assert_raise_message("Set with name Exist and brand Doesnt does not exist") {
+            @test_tracker.get_set("Doesnt", "Exist")
+        }
+    end
 end
 
