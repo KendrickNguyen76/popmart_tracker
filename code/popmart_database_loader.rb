bc7728620cf8c63159ed22c48031f05dc99d12f9
KendrickNguyen76 <kendricknguyen76@gmail.com>
2025-01-22 14:08:22 -0800

    Wrote a bunch of comments for PopMartDBLoader

--- code/popmart_database_loader.rb
+++ code/popmart_database_loader.rb
@@ -6,10 +6,22 @@
 require_relative "popmart_database.rb"
 
 class PopMartDBLoader
+    # PopMartDBLoader is a class that is responsible for loading
+    # and storing informtion to and from PopMartDatabaseHandler
+    # and PopTrackLogic. It acts as a middleman essentially.
+
+    # It has one instance variable and one constant:
+
+    # @db_path - represents the connection to the database that this
+    #            class will interact with through PopMartDatabaseHandler
+    # DB_PATH_DEFAULT - A constant default value for @db_path
+
     attr_reader :db_path
 
     DB_PATH_DEFAULT = "placeholder"
+    
 
+    # Constructor for a PopMartDBLoader object.
     def initialize(provided_db=DB_PATH_DEFAULT)
        @db_path = provided_db 
     end
